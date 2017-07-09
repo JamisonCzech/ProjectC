5a026ea6ad7c430271d2d0074530d8ce57e91784
Jamison Czech <jamisonczech@gmail.com>
2017-07-09 16:13:32 -0500

    Initial commit

--- src/com/edu/metrostate/ics340/j_czech/Main.java
+++ src/com/edu/metrostate/ics340/j_czech/Main.java
@@ -1,8 +1,26 @@
 package com.edu.metrostate.ics340.j_czech;
 
+import static com.edu.metrostate.ics340.j_czech.FileIO.processFile;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        FileIO fileIO = new FileIO();
+
+        int i;
+        String iFile;
+        String oFile;
+
+        iFile = fileIO.chooseFile();
+        if (iFile != null) {
+            i = iFile.lastIndexOf('.');
+            if (i >= 0)
+                oFile = iFile.substring(0, i) + "_out" + iFile.substring(i);
+            else
+                oFile = iFile + "_out";
+            processFile(iFile, oFile);
+        }
+
     }
 }
